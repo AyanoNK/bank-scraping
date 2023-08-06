2ba6926e11dca1d1fbe50f2a631f234dc4b8ea1d
Ayano <s.correa.wilches@gmail.com>
2023-08-06 18:41:27 -0500

    add hover on whole element

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         className={`bg-zinc-50 text-text ${inter.className} flex flex-col min-h-screen items-center justify-center`}
       >
         <main className="flex flex-col items-center justify-center gap-24 p-24 max-w-6xl">
-          <div className="flex flex-row relative items-center">
+          <div className="flex flex-row relative items-center group">
             <h1 className="text-5xl font-bold italic text-center before:block before:absolute before:-inset-7 before:-skew-y-2 before:bg-primary relative inline-block">
               <span className="relative text-white">
                 Bank Statement Scrapper
@@ -30,11 +30,12 @@ export default function RootLayout({
             <Image
               src="/logo.svg"
               alt="Bank Statement Scrapper"
-              className="translate-x-20 -translate-y-4 hover:translate-x-30 hover:translate-y-6 hover:scale-110 ease-in-out duration-300 hover:rotate-45 absolute right-0 hidden md:block"
+              className="translate-x-20 -translate-y-4 group-hover:translate-x-30 group-hover:translate-y-6 group-hover:scale-110 ease-in-out duration-300 group-hover:rotate-45 absolute right-0 hidden md:block"
               width={75}
               height={75}
             />
           </div>
+
           {children}
         </main>
       </body>
