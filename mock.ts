e22bcba8a5bd4a4609c782887c78a7497e3f16a6
Ayano <s.correa.wilches@gmail.com>
2023-08-22 23:13:16 -0500

    removed mock error by committing horrible changes

--- mock.ts
+++ mock.ts
@@ -1,60 +1,60 @@
 // mock considering backend just scraps items and returns them
 
-export const mockResponse: TResponse = {
-  itemsFound: 10,
-  itemsProcessed: 9,
-  totals: [
-    {
-      name: 'RAPPI',
-      price: 350000,
-      month: 8,
-      year: 2023,
-    },
-    {
-      name: 'ENEL',
-      price: 200000,
-      month: 8,
-      year: 2023,
-    },
-    {
-      name: 'UBER',
-      price: 681500,
-      month: 9,
-      year: 2023,
-    },
-    {
-      name: 'CABIFY',
-      price: 222900,
-      month: 9,
-      year: 2023,
-    },
-  ],
-  items: [
-    {
-      name: 'RAPPI',
-      price: 100000,
-      month: 8,
-      year: 2023,
-    },
-    {
-      name: 'ENEL',
-      price: 200000,
-      month: 8,
-      year: 2023,
-    },
-    {
-      name: 'UBER',
-      price: 981500,
-      month: 9,
-      year: 2023,
-    },
-    {
-      name: 'CABIFY',
-      price: 22900,
-      month: 9,
-      year: 2023,
-    },
-  ],
-};
+// export const mockResponse: TResponse = {
+//   itemsFound: 10,
+//   itemsProcessed: 9,
+//   totals: [
+//     {
+//       name: 'RAPPI',
+//       price: 350000,
+//       month: 8,
+//       year: 2023,
+//     },
+//     {
+//       name: 'ENEL',
+//       price: 200000,
+//       month: 8,
+//       year: 2023,
+//     },
+//     {
+//       name: 'UBER',
+//       price: 681500,
+//       month: 9,
+//       year: 2023,
+//     },
+//     {
+//       name: 'CABIFY',
+//       price: 222900,
+//       month: 9,
+//       year: 2023,
+//     },
+//   ],
+//   items: [
+//     {
+//       name: 'RAPPI',
+//       price: 100000,
+//       month: 8,
+//       year: 2023,
+//     },
+//     {
+//       name: 'ENEL',
+//       price: 200000,
+//       month: 8,
+//       year: 2023,
+//     },
+//     {
+//       name: 'UBER',
+//       price: 981500,
+//       month: 9,
+//       year: 2023,
+//     },
+//     {
+//       name: 'CABIFY',
+//       price: 22900,
+//       month: 9,
+//       year: 2023,
+//     },
+//   ],
+// };
 
-export const MockBoxItem = Promise.resolve(mockResponse);
+// export const MockBoxItem = Promise.resolve(mockResponse);
